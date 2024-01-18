b16281b436fd1aa9e9c881a9f62c503b9b44b05c
Leonardo Ramos Anacleto <135238872+lranacleto85@users.noreply.github.com>
2024-01-17 21:20:55 -0300

    Delete logica-js/app.js

--- logica-js/app.js
+++
@@ -1,6 +0,0 @@
-alert("Bem vindo ao jogo do número secreto");
-let numeroSecreto = 9;
-let chute = prompt("Escolha um número entre 1 e 10");
-if (chute == numeroSecreto) {
-	console.log("Parabéns você acertou o número secreto (9)");
-}
